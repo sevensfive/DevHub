1a974554d21b80c689f238c1708c617c3e9b48e8
e <sevensfive7@163.com>
2018-09-22 16:52:41 -0700

    all

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://sevensfive:sevensfive7@ds161312.mlab.com:61312/devhub",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
